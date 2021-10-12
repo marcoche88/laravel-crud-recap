094dc22d697f776f7db4466666d1e860b8d34243
marcoche88 <marco.che@hotmail.it>
2021-10-12 22:19:01 +0200

    modify input form

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    protected $table='movies';
-    protected $fillable = ['title', 'overview'];
+    protected $table = 'movies';
+    protected $fillable = ['title', 'overview', 'date_release', 'genres', 'poster_path'];
 }
--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -9,10 +9,31 @@
 <body>
     <form method='POST' action="{{ route('movies.store') }}">
         @csrf
-        <label for="title">Title</label>
-        <input type="text" name='title' id='title'>
-        <label for="overview">Descrizione</label>
-        <textarea name="overview" id="overview" cols="30" rows="10"></textarea>
+        <div>
+            <label for="title">Titolo</label>
+            <input type="text" name='title' id='title'>
+        </div>
+        <hr>
+        <div>
+            <label for="genres">Genere</label>
+            <input type="text" name='genres' id='genres'>
+        </div>
+        <hr>
+        <div>
+            <label for="date_release">Data di rilascio</label>
+            <input type="text" name='date_release' id='date_release'>
+        </div>
+        <hr>
+        <div>
+            <label for="poster_path">Immagine di copertina</label>
+            <input type="text" name='poster_path' id='poster_path'>
+        </div>
+        <hr>
+        <div>
+            <label for="overview">Descrizione</label>
+            <textarea name="overview" id="overview" cols="30" rows="10"></textarea>
+        </div>
+        <hr>
         <button type="submit">Invia</button>
     </form>
 </body>
